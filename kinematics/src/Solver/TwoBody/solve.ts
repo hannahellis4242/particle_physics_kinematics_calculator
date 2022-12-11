2b370a284f0b07376564a04dcbcee9fc6f022074
hannahellis4242 <hannahellis4242@gmail.com>
2022-12-11 19:24:40 +0000

    Solving rest frame

--- kinematics/src/Solver/TwoBody/solve.ts
+++ kinematics/src/Solver/TwoBody/solve.ts
@@ -3,37 +3,72 @@ import Problem from "../../Model/TwoBody/Problem";
 import RestSolution from "../../Model/TwoBody/RestSolution";
 import Solution from "../../Model/TwoBody/Solution";
 
-/*
-const getMass = 
-
-const solveRestFrameA = (problem:Problem) : Particle =>{
-    const { parent , daughterA , daughterB } = problem;
-
-    const energy =
-      (parent.mass * parent.mass +
-        daughterA.mass * daughterA.mass -
-        daughterB.mass * daughterB.mass) /
-      (2 * parent.mass);
+interface Property {
+  parent(problem: Problem): number;
+  daughterA(problem: Problem): number;
+  daughterB(problem: Problem): number;
 }
 
-const solveRestFrame = (problem: Problem): RestSolution => {
-  const { parent } = problem;
-  const daughterA: Particle = { mass: problem.daughterA.mass };
-  let daughterB: Particle = { mass: problem.daughterB.mass };
-  {
-    const energy =
-      (parent.mass * parent.mass +
-        daughterA.mass * daughterA.mass -
-        daughterB.mass * daughterB.mass) /
-      (2 * parent.mass);
-    daughterA = { energy, ...daughterA };
+class Mass implements Property {
+  parent({ parent }: Problem) {
+    return parent.mass;
   }
-  return { daughterA, daughterB };
-};
-*/
-const solve = async (problem: Problem): Promise<Solution> => {
-  //step 1 sovle in lab frame
+  daughterA({ daughterA }: Problem) {
+    return daughterA.mass;
+  }
+  daughterB({ daughterB }: Problem) {
+    return daughterB.mass;
+  }
+}
 
+const getProperty =
+  (p: Property) => (k: keyof Property) => (problem: Problem) =>
+    p[k](problem);
+
+const solveRestFrameA = (problem: Problem): Particle => {
+  const { parent, daughterA, daughterB, centreOfMassDecayAngle } = problem;
+  const energy =
+    (parent.mass * parent.mass +
+      daughterA.mass * daughterA.mass -
+      daughterB.mass * daughterB.mass) /
+    (2 * parent.mass);
+  const momentum = Math.sqrt(energy * energy - daughterA.mass * daughterA.mass);
+  const x = momentum * Math.cos(centreOfMassDecayAngle);
+  const y = momentum * Math.sin(centreOfMassDecayAngle);
+  return {
+    mass: daughterA.mass,
+    energy,
+    momentum: { magnitude: momentum, x, y, z: 0 },
+  };
+};
+
+const solveRestFrameB = (problem: Problem): Particle => {
+  const { parent, daughterA, daughterB, centreOfMassDecayAngle } = problem;
+  const energy =
+    (parent.mass * parent.mass +
+      daughterB.mass * daughterB.mass -
+      daughterA.mass * daughterA.mass) /
+    (2 * parent.mass);
+  const momentum = Math.sqrt(energy * energy - daughterB.mass * daughterB.mass);
+  const x = -momentum * Math.cos(centreOfMassDecayAngle);
+  const y = -momentum * Math.sin(centreOfMassDecayAngle);
+  return {
+    mass: daughterB.mass,
+    energy,
+    momentum: { magnitude: momentum, x, y, z: 0 },
+  };
+};
+
+const solveRestFrame = (problem: Problem): RestSolution => {
+  return {
+    daughterA: solveRestFrameA(problem),
+    daughterB: solveRestFrameB(problem),
+  };
+};
+
+const solve = async (problem: Problem): Promise<Solution> => {
+  //step 1 sovle in rest frame
+  const rest = solveRestFrame(problem);
   return {};
 };
 
